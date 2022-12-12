753364dbc74bb266d2870da1ab147414cb38a14d
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-11 22:37:12 -0800

    Add new files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 1 (Success)
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
-		char *mgs = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	char *mgs = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
 
-			write(STDOUT_FILENO, mgs, 59);
+	write(STDOUT_FILENO, mgs, 59);
 
-				return (1);
+	return (1);
 }
