561b7398e9e9fa1ef82650513a526c173ef64ac5
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-13 07:04:14 -0800

    Add new files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs -  computes the absolute value of an integer
+ * @int: Argument
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+	return (int < 0 ? -int : int);
+}
