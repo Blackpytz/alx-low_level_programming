1eecda0a18181f9955d7cd325f806ac3e5b22901
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-13 04:39:19 -0800

    Add new files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,6 @@
 void print_alphabet(void);
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - prints the alphabet
  */
 
