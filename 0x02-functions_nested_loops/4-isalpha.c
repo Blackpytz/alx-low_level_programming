f3bce3c035ab87cdcec7863c2c79708c8f9a4067
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-13 04:53:05 -0800

    Add new files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha -  checks for alphabetic character.
+ * @c: Argument
+ *
+ * Return: Always 0 or 1.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
