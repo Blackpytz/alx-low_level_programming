a99c7b4b1e75b61a2cba93d19bbad912351a712a
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-14 07:44:45 -0800

    Add new files

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Nothing.
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			printf("%d, ", k);
+		}
+		putchar('\n');
+	}
+}
