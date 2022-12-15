84d5d9a6867d7c94e818c2897928540f9af64560
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-15 03:37:13 -0800

    Add new fikes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		_putchar('\n');
+		n++;
+	}
+	while (n >= 98)
+	{
+		printf("%d, ", n);
+		_putchar('\n');
+		n--;
+	}
+	return (0);
+}
