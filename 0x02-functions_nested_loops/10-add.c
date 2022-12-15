3d22ea8710836a8149203aba61a8130cf75ef9f4
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-15 02:35:14 -0800

    Add new files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integer and returns the result
+ * @a: Argument
+ * @b: Argument
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
