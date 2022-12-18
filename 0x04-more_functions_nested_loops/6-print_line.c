525745b0a46debb41a569098d7992f0a156482ad
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-18 05:22:34 -0800

    Add new files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: Argument
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x <= n; x++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
