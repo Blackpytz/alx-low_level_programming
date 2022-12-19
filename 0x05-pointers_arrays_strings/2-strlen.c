a2ad12b8753ba4abe200e9431c560765b864d57d
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-19 07:29:35 -0800

    Add new files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: Parameter
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int var;
+
+	var = sizeof(s) - 1;
+
+	return (var);
+}
