b34b9a4c032c14be7ab730a6d7e7d833ce81538b
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-20 08:06:15 -0800

    Add new file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,21 @@ int main(void)
 
 	while (x <= 100)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if (x % 5 == 0)
+		if (x == 100)
 		{
 			printf("Buzz ");
+		}
+		else if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
 		}
 		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
