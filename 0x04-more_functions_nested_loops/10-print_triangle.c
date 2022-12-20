5ec768438f549e2b734c795f4f195a18990335c3
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-20 08:17:11 -0800

    Add new files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,9 +12,13 @@ void print_triangle(int size)
 	int x, y;
 
 	for (x = 0; x < size; x++)
-		for (y = 0; y < size; y++)
+		for (y = 0; y > size; y++)
 		{
-			if (x + y < y - 1)
+			if (size <= 0)
+			{
+				putchar('\n');
+			}
+			else if (x + y < y - 1)
 			{
 				putchar("#");
 			}
