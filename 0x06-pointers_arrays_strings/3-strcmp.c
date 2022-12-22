d7163ef50f85d13bef479596ea10549621e395ad
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-22 07:32:59 -0800

    Add new file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: Parameter
+ * @s2: Parameter
+ *
+ * Return 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
