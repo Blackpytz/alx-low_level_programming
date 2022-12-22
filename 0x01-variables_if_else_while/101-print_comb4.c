821e38894204cf97246d94a583782c61cbc9ec02
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-22 11:36:40 -0800

    Add new file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - prints all possible different
+ * combinations of three digits.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a, b, c = 48;
+
+	while (a < 58)
+	{
+		while (b < 58)
+		{
+			while (c < 58)
+			{
+				if (a != b && b != i && a != i && a < b && b < c)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(c);
+					if (!(a == 55 && b == 56 && c == 57))
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				c++;
+			}
+			b++;
+		}
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
