e693215ba52bed7bfd8f47758d8dab75ffceb60c
Reid Chikezie <theophiluschikezie@gmail.com>
2022-12-28 05:03:24 -0800

    Add new file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ * starting with 1
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	unsigned long int i, j = 0, t1 = 1, t2 = 2;
+
+	for (i = 0; i <= 50; i++)
+	{
+		printf("%ld, ", t1);
+		j = t1 + t2;
+		t1 = t2;
+		t2 = j;
+	}
+	putchar('\n');
+
+	return (0);
+}
