6567feb58671cd9fc4771c66f84bef6bc4364acd
Reid Chikezie <theophiluschikezie@gmail.com>
2023-01-04 07:54:01 -0800

    Add new file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: Oarameter
+ *
+ * Return: Alwayd 0 (suucess).
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
