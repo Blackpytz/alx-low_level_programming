335f47e8a7b664b79802dec01d95ff9260f2e981
Reid Chikezie <theophiluschikezie@gmail.com>
2023-01-08 06:10:09 -0800

    Add new file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,12 @@
  * @argc: Argument count.
  * @argv: Argument vector.
  *
- * Return: Always 0.
+ * Return: Always 0 (sucess).
  */
 
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
