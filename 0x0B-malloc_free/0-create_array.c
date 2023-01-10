7edc7a12f1be8c5ac7333acd1b00d4726122635a
Reid Chikezie <theophiluschikezie@gmail.com>
2023-01-10 08:07:20 -0800

    Add new file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,13 @@ char *create_array(unsigned int size, char c)
 	a = malloc(sizeof(char) * size);
 	if (size == 0)
 	{
-		return (NULL);
+		return (1);
 	}
 	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
 	}
+	free(a);
 
 	return (a);
 }
