847408f58938936ea2cc7fb01a1aedbb7d11d70f
Blackpytz <theophiluschikezie@gmail.com>
2023-05-21 09:54:30 -0700

    Add new file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+	int alphabets = 97;
+
+	while (alphabets <= 122)
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	alphabets = 65;
+	
+	while (alphabets <= 90)
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	putchar('\n');
+
+	return (0);
+}
