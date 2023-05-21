881d58dbdcedc67d0a267b16aaed20e920811a87
Blackpytz <theophiluschikezie@gmail.com>
2023-05-21 10:30:17 -0700

    Add new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combo of single-digit numbers.
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+	int nums = 48;
+
+	while (nums <= 57)
+	{
+		putchar(nums);
+		putchar(',');
+		putchar(' ');
+		nums++;
+	}
+	putchar('\n');
+
+	return (0);
+}
