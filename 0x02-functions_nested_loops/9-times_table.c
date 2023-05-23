2bb60edc4ab10f388b877886ed28c20acbca013b
Blackpytz <theophiluschikezie@gmail.com>
2023-05-23 06:23:29 -0700

    Add new file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 
 	for (height = 0; height <= 9; height++)
 	{
-		for (; width <= 9; height++)
+		for (; width <= 9; width++)
 		{
 			ttable = height * width;
 			_putchar('0' + ttable / 10);
