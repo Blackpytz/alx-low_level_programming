d66d37578537bdec3df54fe59046d4d012c41ce9
Blackpytz <theophiluschikezie@gmail.com>
2023-05-23 04:06:45 -0700

    Add new file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * @c: Parameter.
+ *
+ * Return: 1 if c is lowercase,
+ * OR 0 otherwise.
+ */
+int _islower(int c)
+{
+	char alphabets = 97;
+
+	while (alphabets <= 122)
+	{
+		if (c == alphabets)
+			return (1);
+	}
+	return (0);
+}
