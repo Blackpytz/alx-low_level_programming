32af0f9f0b8acd0f3f3eee3114f94c9305dc6554
Blackpytz <theophiluschikezie@gmail.com>
2023-05-24 01:15:00 -0700

    Add new file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -29,8 +29,7 @@ void times_table(void)
 				_putchar('0' + ttable / 10);
 				_putchar('0' + ttable % 10);
 			}
-			if (width != 9)
-				_putchar(',');
+			_putchar(',');
 			_putchar(' ');
 		}
 			_putchar('\n');
