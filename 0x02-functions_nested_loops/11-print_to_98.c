2d8216e79e19950ca14f357e7ebfeb74f8587ed7
Blackpytz <theophiluschikezie@gmail.com>
2023-05-24 09:45:46 -0700

    Add new file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,14 +16,14 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (num = n; num > 98; --num)
+		for (num = n; num >= 98; --num)
 		{
 			printf("%d, ", num);
 		}
 	}
 	else
 	{
-		for (num = n; num < 98; ++num)
+		for (num = n; num <= 98; ++num)
 		{
 			printf("%d, ", num);
 		}
