f4ab76969424dfcc5b57df7e1f8c882b11913db7
Blackpytz <theophiluschikezie@gmail.com>
2023-05-29 21:14:04 -0700

    Add new file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_rev - prints a string, in reverse
+*@s: pointer to the string to be reversed
+*Return: void.
+*/
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; len[s] != '\0'; len++)
+	{}
+	while (len--)
+		_putchar(len[s]);
+	_putchar('\n');
+}
