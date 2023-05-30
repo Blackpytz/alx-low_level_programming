852da6699f3c9d83faacec2a1530b9b6359c7443
Blackpytz <theophiluschikezie@gmail.com>
2023-05-30 10:24:43 -0700

    Add new file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,24 @@
 #include "main.h"
 
 /**
-*_atoi - c
+*_atoi - convert a string to an integer
+*@s: pointer to a string to be converted to integer
+*Return: result of the converted string to integer.
+*/
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign = -sign;
+		else if (s[i] >= 48 && s[i] <= 57)
+			result = result * 10 + (s[i] - '0');
+		else if (result > 0)
+			break;
+		i++;
+	}
+
+	return (sign * result);
+}
