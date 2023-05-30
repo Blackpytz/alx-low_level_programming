6a636a63a057fc51e577b4c32bd3faee2566b66a
Blackpytz <theophiluschikezie@gmail.com>
2023-05-29 21:03:46 -0700

    Add new file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts - prints a string
+*@str: pointer to the string to print
+*Return: null.
+*/
+void _puts(char *str)
+{
+	int len = 0;
+
+	while(len[str] != '\0')
+	{
+		putchar(len[str]);
+		len++;
+	}
+}
