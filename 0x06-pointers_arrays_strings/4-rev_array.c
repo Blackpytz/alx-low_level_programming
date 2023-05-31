360fe662517391d8ec286e78f3cfbecb2c22426e
Blackpytz <theophiluschikezie@gmail.com>
2023-05-31 06:33:02 -0700

    Add new file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, temp;
 
-	for (i = 0; a[i] != '\0'; i++)
-	{}
-	while (n--)
+	for (i = 0; i < n / 2; i++)
 	{
-		a[i--];
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
 }
