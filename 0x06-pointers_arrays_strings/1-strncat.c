119d8dea67e7be915a5b9d602f7bb56b9f98e3ce
Blackpytz <theophiluschikezie@gmail.com>
2023-05-31 05:26:14 -0700

    Add new file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strncat - concatenates two strings
+*@dest: pointer to the string to be overwritten
+*@src: pointer to the string to be appended to the string dest
+*@n: bytes from src
+*Return: A pointer to the resulting string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len = 0;
+
+	while (src[dest_len] != '\0')
+		dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i + 1] = '\0';
+
+	return (dest);
+}
