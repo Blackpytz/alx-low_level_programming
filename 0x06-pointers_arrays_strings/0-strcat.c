6f1ed271989527b81d9d5c2bb298c41388a6649b
Blackpytz <theophiluschikezie@gmail.com>
2023-05-31 05:14:07 -0700

    Add new file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,5 +8,13 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int len;
+	int i, len = 0;
+
+	while (len[dest] != '\0')
+		len++;
+	for (i = 0; i[src] != '\0'; i++)
+		dest[len + i] = src[i];
+	dest[len + i + 1] = '\0';
+
+	return (dest);
 }
