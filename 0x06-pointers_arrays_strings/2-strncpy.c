23cc35b01e44e9dcb7b7e86b4f0e650c85ec01de
Blackpytz <theophiluschikezie@gmail.com>
2023-05-31 05:45:53 -0700

    Add new file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strncpy - copies a string
+*@dest: pointer to the string to be overwritten
+*@src: pointer to the string to be copied to the string dest
+*@n: bytes from src
+*Return: A pointer to the resulting dest.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
