b5554c505a685606c857050472a8f45dc698f61c
Blackpytz <theophiluschikezie@gmail.com>
2023-05-31 10:41:38 -0700

    Add new file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; i++, j++)
 	{
-		if (s[1] < s[2])
+		if (s1[i] < s2[i])
 			return (s1[i] - s2[i]);
 		else if (s1[i] > s2[i])
 			return (s1[i] - s2[i]);
