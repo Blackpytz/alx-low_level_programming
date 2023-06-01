6931f97d1c1898566d3cabe10f22520dc06c8a10
Blackpytz <theophiluschikezie@gmail.com>
2023-06-01 00:03:01 -0700

    Add new file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*leet - encodes a string into 1337
+*@str: pointer to the string to be encoded
+*Return: a pointer to the encodeed string
+*/
+char *leet(char *str)
+{
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; J++)
+		{
+			if (str[i] == s1[j])
+				str[i] = s2[j];
+		}
+	}
+
+	return (str);
+}
