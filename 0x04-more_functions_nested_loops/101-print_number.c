f1142b352d83a70d8e1e7ae65f6d0f564aaf5dad
Blackpytz <theophiluschikezie@gmail.com>
2023-06-06 21:49:04 -0700

    Add new file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_number - function that prints an integer.
+*@n: parameter
+*Return: On success 0.
+*/
+void print_number(int n)
+{
+	int negative = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		negative -= n;
+	}
+
+	if (n == 0)
+		putchar('0');
+
+	if (n / 10)
+	{
+		print_number(n / 10)
+	}
+	_putchar('0' + (num % 10));
+}
