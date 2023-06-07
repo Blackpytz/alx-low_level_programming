567b699a05e0788e59d497e2e126f15ced82f2ec
Blackpytz <theophiluschikezie@gmail.com>
2023-06-06 19:05:04 -0700

    Add new file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,13 @@
 */
 char *_strchr(char *s, char c)
 {
-	char *str;
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (*(s + i) == c)
 		{
-			str = s[i];
-			return (s);
+			return (s + i);
 		}
 	}
 	return (NULL);
