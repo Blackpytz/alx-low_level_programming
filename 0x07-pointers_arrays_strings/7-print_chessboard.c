fed4ede0a16674f698e61cdeddf20c5705ab2771
Blackpytz <theophiluschikezie@gmail.com>
2023-06-06 21:32:41 -0700

    Add new file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 1; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 1; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			putchar(a[i][j]);
 		}
