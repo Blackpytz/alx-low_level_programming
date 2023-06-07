2d1ed8d0181fb5c376fd8d499dc99a9d6b695d71
Blackpytz <theophiluschikezie@gmail.com>
2023-06-06 19:38:40 -0700

    Add new file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, num = 0;
+	int match;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		match = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				num++;
+				match = 1;
+				break;
 			}
-			break;
 		}
+		if (match == 0)
+			break;
 	}
 	return (num);
 }
