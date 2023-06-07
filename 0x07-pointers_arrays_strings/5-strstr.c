7d094c9409d877bfdca0716afdca9a158f99bf53
Blackpytz <theophiluschikezie@gmail.com>
2023-06-06 20:22:52 -0700

    Add new files

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,15 +12,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (*needle == '\0')
+		return (haystack);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i] == needle[j])
-			{
-				return (haystack + i);
-			}
-		}
+		
 	}
 
 	return (NULL);
