382c63ec5b2fe231a42ca927841919c5a638e0f9
Blackpytz <theophiluschikezie@gmail.com>
2023-06-09 20:15:38 -0700

    Add new file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,8 +9,11 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		putchar(*(s + 1));
-		_puts_recursion(*s);
+		putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
