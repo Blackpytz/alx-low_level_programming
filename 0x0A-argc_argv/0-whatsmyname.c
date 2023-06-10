85edf8fe0518b04209e0807ed32738af57393351
Blackpytz <theophiluschikezie@gmail.com>
2023-06-09 17:30:33 -0700

    Add new file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - program that prints its name
+*@argc: argument count
+*@argv: argument vector
+*Return: 0 on success.
+*/
+int main(int argc, char *argv[])
+{
+	printf("%c\n", argv[0]);
+
+	return (0);
+}
