4c93a39a0183c0c23f423afab8eea285c02e16bb
Blackpytz <theophiluschikezie@gmail.com>
2023-06-09 18:30:01 -0700

    Add new file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,11 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]), num2 = atoi(argv[2]), result = num1 * num2;
 
-	if (argc > 1)
-	{
-		printf("%d\n", result);
-	}
-	else if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
 	}
+	printf("%d\n", result);
 
 	return (0);
 }
