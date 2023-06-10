bc938b56ad0795da63f5b61ffcf7cddc4a96a2f2
Blackpytz <theophiluschikezie@gmail.com>
2023-06-09 20:10:37 -0700

    Add new file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_puts_recursion - function that prints a string
+*@s: pointer to a sting
+*Return: void.
+*/
+void _puts_recursion(char *s)
+{
+	if(*s)
+	{
+		putchar(*s);
+		_puts_recursion(*(s + 1));
+	}
+	_putchar('\n');
+}
