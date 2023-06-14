551a3c9aa0e3cdcf3250076d399845a5b7f8253a
Blackpytz <theophiluschikezie@gmail.com>
2023-06-14 15:45:36 -0700

    Add new file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* argstostr - function that concatenates all the arguments of your program
+* @ac: argument count
+* @av: argument vector
+* Return: A pointer to a new string, or NULL if it fails.
+*/
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, j;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+}
