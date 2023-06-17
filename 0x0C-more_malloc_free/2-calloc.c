02f9852fd4ebb86a0508c6f10ad4f4f8ed7f99b6
Blackpytz <theophiluschikezie@gmail.com>
2023-06-16 18:26:48 -0700

    Add new file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	void *ptr = malloc(nmemb * size);
-	char *char_ptr = ptr;
-	unsigned int i;
-
+	void *ptr;
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	char *char_ptr = ptr;
+	unsigned int i;
 
 	for (i = 0; i < nmemb * size; i++)
 		char_ptr[i] = 0;
