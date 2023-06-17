2dcde87266260694ac1a43d616ffcb57d9487036
Blackpytz <theophiluschikezie@gmail.com>
2023-06-16 17:32:49 -0700

    Add new file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* malloc_checked - function that allocates memory using malloc
+* @b: parameter
+*
+* Return: A pointer to the allocated memory,
+* or 98 on failure.
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (98);
+
+	return (ptr);
+}
