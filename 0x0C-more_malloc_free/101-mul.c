3e3b96773b7b71e00980b85fb7386e734b0aa60c
Blackpytz <theophiluschikezie@gmail.com>
2023-06-16 20:09:36 -0700

    Add new file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
-* _print - moves a string one place to the left and prints the string
-* @str: string to move
-* @l: size of string
-*
-* Return: void.
-*/
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
 void _print(char *str, int l)
 {
 	int i, j;
@@ -19,7 +20,7 @@ void _print(char *str, int l)
 		if (str[i] != '0')
 			j = 1;
 		if (j || i == l - 1)
-			putchar(str[i]);
+			_putchar(str[i]);
 		i++;
 	}
 
@@ -28,15 +29,15 @@ void _print(char *str, int l)
 }
 
 /**
-* mul - multiplies a char with a string and placesthe answer into dest
-* @n: char to multiply
-* @num: string to multiply
-* @num_index: last non NULL index of num
-* @dest: destination of multiplication
-* @dest_index: highest indes to start addition
-*
-* Return: pointer to dest, or NULL on failure
-*/
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
 	int j, k, mul, mulrem, add, addrem;
@@ -63,11 +64,11 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 	return (dest);
 }
 /**
-* check_for_digits - checks the arguments to ensure they are digits
-* @av: pointer to arguments
-*
-* Return: 0 if digits, 1 if not.
-*/
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
+ */
 int check_for_digits(char **av)
 {
 	int i, j;
@@ -84,12 +85,12 @@ int check_for_digits(char **av)
 }
 
 /**
-* init - initializes a sting
-* @str: string to initialize
-* @l: length of string
-*
-* Return: void
-*/
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
+ *
+ * Return: void
+ */
 void init(char *str, int l)
 {
 	int i;
@@ -100,12 +101,12 @@ void init(char *str, int l)
 }
 
 /**
-* main - multiply two numbers
-* @argc: number of arguments
-* @argv: argument vector
-*
-* Return: zero, or exit status of 98 if failure
-*/
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: zero, or exit status of 98 if failure
+ */
 int main(int argc, char *argv[])
 {
 	int l1, l2, ln, ti, i;
@@ -116,29 +117,29 @@ int main(int argc, char *argv[])
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
-			putchar(e[ti]);
+			_putchar(e[ti]);
 		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
 		;
-	for (l2 = 0; argb[2][l2]; l2++)
+	for (l2 = 0; argv[2][l2]; l2++)
 		;
 	ln = l1 + l2 + 1;
 	a = malloc(ln * sizeof(char));
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
-			putchar(e[ti]);
+			_putchar(e[ti]);
 		exit(98);
 	}
 	init(a, ln - 1);
-	for (ti = l2, i = 0; ti >= 0; ti--, i++)
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
-				putchar(e[ti]);
+				_putchar(e[ti]);
 			free(a);
 			exit(98);
 		}
