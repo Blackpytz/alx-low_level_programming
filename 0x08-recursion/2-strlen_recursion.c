2ce86016b5e5ee5a886bde249807d5cbe6955c09
Blackpytz <theophiluschikezie@gmail.com>
2023-07-05 16:12:44 -0700

    Add new file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,14 +9,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 1;
+	int len = 0;
 
-	if (*s++)
+	if (*s)
 	{
-		len = len + _strlen_recursion(s + 1);
 		len++;
+		len = len + _strlen_recursion(s + 1);
 	}
-	else
-		len--;
 	return (len);
 }
