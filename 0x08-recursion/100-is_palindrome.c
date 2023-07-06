05fb17fa340c1c234ccd7c7a80b4c04c3d925621
Blackpytz <theophiluschikezie@gmail.com>
2023-07-06 11:43:50 -0700

    Add new files

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,11 +11,8 @@
 */
 int is_palindrome_helper(char *s, int start, int end)
 {
-	if (start == end)
-		return (1);
-	if (s[start] != s[end])
-		return (0);
-
+	if (start == end) return (1);
+	if (s[start] != s[end]) return (0);
 	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
@@ -32,6 +29,5 @@ int is_palindrome(char *s)
 	length = strlen(s);
 	if (length <= 1)
 		return (1);
-
 	return (is_palindrome_helper(s, 0, length - 1));
 }
