4aa416c1d55c46a8287fb7016b9b312ec0363dcf
Blackpytz <theophiluschikezie@gmail.com>
2023-07-06 11:45:37 -0700

    Add new files

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,20 +6,19 @@
 *@s: string to check if it's palindrome
 *@start: start of string
 *@end: end of string
-*
 *Return: 1 if string is a palindrome OR 0 if not.
 */
 int is_palindrome_helper(char *s, int start, int end)
 {
-	if (start == end)return (1);
-	if (s[start] != s[end])return (0);
+	if (start == end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
 	return (is_palindrome_helper(s, start + 1, end - 1));
 }
-
 /**
 *is_palindrome - function that returns 1 if string is a palindrome and 0 if not
 *@s: string to be checked if it is palindrome
-*
 *Return: 1 if string is a palindrome OR 0 if not.
 */
 int is_palindrome(char *s)
