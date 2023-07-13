546b7aaaf4cd04dd6258552fd460bc2efed89723
Blackpytz <theophiluschikezie@gmail.com>
2023-07-13 07:04:20 -0700

    Add new file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = strlen(s2);
 
 	/* Adjust n if it exceeds the length of s2 */
-	if (n >= len2);
+	if (n >= len2)
 		n = len2;
 
 	total_len = len1 + n + 1;
