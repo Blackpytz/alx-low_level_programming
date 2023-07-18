c84e1bd874a26d3198543b44edb7394c667657d4
Blackpytz <theophiluschikezie@gmail.com>
2023-07-17 20:03:05 -0700

    Add new file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int len1, len2;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
+	
+	d->name = malloc((sizeof(char) * len1)+1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = malloc((sizeof(char) * len2)+1);
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
 
 	strcpy(d->name, name);
 	d->age = age;
