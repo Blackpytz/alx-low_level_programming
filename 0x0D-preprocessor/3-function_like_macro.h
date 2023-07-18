7209035096f5235ea87bcafa32d9cf4a958b2470
Blackpytz <theophiluschikezie@gmail.com>
2023-07-17 20:43:48 -0700

    Add new file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef 3-FUNCTION_LIKE_MACRO_H
-#define 3-FUNCTION_LIKE_MACRO_H
+#ifndef 3 - FUNCTION_LIKE_MACRO_H
+#define 3 - FUNCTION_LIKE_MACRO_H
+
 #define ABS(X) ((X) > (0) ? (X) : (-X))
 
 #endif /* ABS */
