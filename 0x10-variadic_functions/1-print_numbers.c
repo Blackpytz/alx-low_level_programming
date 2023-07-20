adbe413b98ca1b6ca59de4b02665f030b3128851
Blackpytz <theophiluschikezie@gmail.com>
2023-07-20 09:52:38 -0700

    Add new file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
-		return (0);
+		return;
 	if (separator == NULL)
 		return;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		printf("%s%s", va_arg(ap, int), ++*separator);
+		printf("%s", va_arg(ap, int));
 
 	va_end(ap);
 	putchar('\n');
