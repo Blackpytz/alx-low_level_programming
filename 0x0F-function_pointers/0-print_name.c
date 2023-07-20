04904ff26c10b41a70081254cc7c4a8458c97996
Blackpytz <theophiluschikezie@gmail.com>
2023-07-19 18:34:09 -0700

    Add new file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,13 @@
-f(name + i);
+#include "function_pointers.h"
+
+/**
+* print_name - function that prints a name
+* @name: pointer to an array of character
+* @f: pointer to a function
+*
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name + i);
 }
