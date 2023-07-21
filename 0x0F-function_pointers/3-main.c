baf707afeaa609597dc71257b098cd924a878d6d
Blackpytz <theophiluschikezie@gmail.com>
2023-07-21 12:26:58 -0700

    Add new file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "calc.h"
+
+/**
+* main - perform simple operations
+* @argc: argument count
+* @argv: argumetn vector
+*
+* Return: on sucess 1.
+*/
+int main(int __attribute__((__unused__))argc, char argv[])
+{
+	char *operator;
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[2]);
+	num2 = atoi(argv[4]);
+	operator = argv[3];
+
+	if (operator != '+' || operator != '-' || operator != '*' ||
+		operator != '/' || operator != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operator == '%' && num2 == 0) || (*operator == '/' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_function(operator), num1, num2);
+
+	return (1);
+}
