17afa1ebb7e0c5b11cff260837ac301d45ffde9b
Blackpytz <theophiluschikezie@gmail.com>
2023-07-21 12:37:54 -0700

    Add new file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,7 +7,7 @@
 *
 * Return: on sucess 0.
 */
-int main(int __attribute__((__unused__))argc, char argv[])
+int main(int __attribute__((__unused__))argc, char **argv)
 {
 	char *operator;
 	int num1, num2;
