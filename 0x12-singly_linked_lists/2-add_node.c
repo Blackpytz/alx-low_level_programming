b742c20ca45ed80cdb06368c49efe6e2b401577e
Blackpytz <theophiluschikezie@gmail.com>
2023-07-28 15:57:12 -0700

    Add new file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,19 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	new = malloc(sizeof(list_t *));
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->next = *head->next;
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = *head;
+	*head = new;
 
 	return (new);
 }
