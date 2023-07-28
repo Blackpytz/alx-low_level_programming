5ed687428c041e0b006514c5933d6a082e136ccf
Blackpytz <theophiluschikezie@gmail.com>
2023-07-28 15:44:32 -0700

    Add new file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* add_node - function that adds a new node at the beginning of a list_t list
+* @head: pointer to a struct linked list
+* @str: pointer to a string to be duplicated
+*
+* Return: the address of the new element, or NULL if it failed.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->next = head;
+
+	return (new);
+}
