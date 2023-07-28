7f8a23e6eb3581077e7638bac76513e319277430
Blackpytz <theophiluschikezie@gmail.com>
2023-07-28 16:38:10 -0700

    Add new file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *current;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -23,7 +23,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	new->len = strlen(str);
-	new->next = head->next;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		 *head = new;
+	}
+	else
+	{
+		
 
 	return (new);
 }
