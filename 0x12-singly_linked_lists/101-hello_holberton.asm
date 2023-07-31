5174f8c584b21f9133200ddb14de679dfdf49f55
Blackpytz <theophiluschikezie@gmail.com>
2023-07-31 06:34:15 -0700

    Add new file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,9 @@
-section .data
-    hello_msg db 'Hello, Holberton', 0
-    new_line db 10, 0
-
-section .text
-    global main
-    extern printf
-
+       global    main
+          extern    printf
 main:
-    ; Calling printf with the hello_msg
-    mov rdi, hello_msg
-    call printf
-
-    ; Calling printf with the new_line
-    mov rdi, new_line
-    call printf
-
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; exit status 0
-    syscall
-
-    ret
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0 
