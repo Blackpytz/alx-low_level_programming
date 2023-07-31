9332d8018965ead6d463a3e691e4206328be516b
Blackpytz <theophiluschikezie@gmail.com>
2023-07-31 06:26:48 -0700

    Add new file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* free_list - function that frees a list_t list
+* @head: a singly linked list to be freed
+*
+* Return: void.
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str)
+		free(head);
+		head = temp;
+	}
+}
