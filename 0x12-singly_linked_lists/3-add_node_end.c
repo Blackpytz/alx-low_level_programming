b56cbb59be08f8dfbebc6d4881caa89d9480349c
Blackpytz <theophiluschikezie@gmail.com>
2023-07-31 06:16:10 -0700

    Add new file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,7 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		current = *head;
-		while (current->next != NULL)
+		while (current != NULL)
 		{
 			current = current->next;
 		}
