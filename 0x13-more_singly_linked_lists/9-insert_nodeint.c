00ed1a9ce829c20c028f7f963b5c98f3d3cae3d3
Blackpytz <theophiluschikezie@gmail.com>
2023-08-01 11:40:57 -0700

    Add new file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (; i < idx - 1 && current->next != NULL; i++, current = current->next)
+	for (; i < idx - 1 && current != NULL; i++, current = current->next)
 	{
 	}
 
