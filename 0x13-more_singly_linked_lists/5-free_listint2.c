731f6f6bdb7f0a1dc6154062a5765d101f5e7607
Blackpytz <theophiluschikezie@gmail.com>
2023-08-01 05:01:02 -0700

    Add new file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,13 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp = *head, *axu;
 
 	while (head != NULL)
 	{
-		temp = *head->next;
-		free(head);
-		*head = temp;
+		axu = temp->next;
+		free(temp);
+		temp = axu;
 	}
 	*head = NULL;
 }
