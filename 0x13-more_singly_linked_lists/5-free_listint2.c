3ea8785d5a8b2506351049cd56451d527656f951
Blackpytz <theophiluschikezie@gmail.com>
2023-08-01 04:57:37 -0700

    Add new file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - function that frees a listint_t list
+* @head: singly linked list
+*
+* Return: Nothing.
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t* temp;
+
+	while (head != NULL)
+	{
+		temp = *head->next;
+		free(head);
+		*head = temp;
+	}
+	*head = NULL;
+}
