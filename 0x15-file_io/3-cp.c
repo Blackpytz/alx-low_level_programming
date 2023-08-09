f9a787ceb2cf373a4dc52a10159ad2463d0fba9a
Blackpytz <theophiluschikezie@gmail.com>
2023-08-08 20:07:44 -0700

    Add new file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void copy_text(const char *file from, char *file_to);
+
+/**
+* main - main function
+* @ac: argument count
+* @av: argument vector
+*
+* Return: Always 0.
+*/
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprinf(2, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	copy 
+}
