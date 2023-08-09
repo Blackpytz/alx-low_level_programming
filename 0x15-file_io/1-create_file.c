d70d6f1f002001a3ac739d851ceb8a11b176720b
Blackpytz <theophiluschikezie@gmail.com>
2023-08-08 19:03:52 -0700

    Add new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[len] != NULL)
+	while (text_content[len])
 		len++;
 
 	fd = open(filename, O_CREAT | O_WRONLY, mode);
