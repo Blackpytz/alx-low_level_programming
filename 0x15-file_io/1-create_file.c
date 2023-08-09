bf3668a9740ead119531f9fc31d4c4394f1d2270
Blackpytz <theophiluschikezie@gmail.com>
2023-08-08 19:06:48 -0700

    Add new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	len = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY, mode);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, mode);
 	if (fd == -1)
 	{
 		return (-1);
