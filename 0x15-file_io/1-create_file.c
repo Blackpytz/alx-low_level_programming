2c324e14c5baba5c06ee5041849b2266a883f76b
Blackpytz <theophiluschikezie@gmail.com>
2023-08-08 18:54:57 -0700

    Add new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* create_file - function that creates a file
+* @filename: name of the file to create
+* @text_content: string to wite to the file
+*
+* Return: 1 on succes, -1 on failue
+*/
+int create_file(const char *filename, char *text_content)
+{
+	char *buffer;
+	int fd, len = 0, i = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (*text_content++)
+		len++;
+	buffer = malloc(sizeof(char) * len + 1);
+	if (buffer == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT, mode);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (-1);
+	}
+	while (text_content[i] != NULL)
+	{
+		buffer[i] = text_content[i];
+		i++;
+	}
+	write(STDOUT_FILENO, buffer, len);
+
+	free(buffer);
+	close(fd);
+	return (1);
+}
