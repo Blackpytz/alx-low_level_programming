2b4576eeb7d1ca737a36e9ef15fac8684c09066d
Blackpytz <theophiluschikezie@gmail.com>
2023-08-10 07:26:07 -0700

    Add new file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,14 +8,31 @@
 *
 * Return: Always 0.
 */
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
 	int fd1, fd2, n;
 	char buf[BUFSIZE];
 	mode_t mode = S_IRUSR | S_IWURS | S_IRGRP | S_IWGRP | S_IROTH;
+
 	if (ac != 3)
 	{
 		dprinf(2, "Usage: cp file_from file_to");
 		exit(97);
 	}
+	if ((fd1 = open(av[1], O_RDONLY, 0)) == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if ((fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, mode)) == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((n = read(fd1, buf, BRFSIZE)) > 0)
+		write(fd2, buf, n);
+	if ((close(fd1) == -1) || (close(fd2) == -1))
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+	return (0);
 }
