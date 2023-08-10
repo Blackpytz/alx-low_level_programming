3ccf266ab34f2fe4c34bf3cfc1c292015f45d72c
Blackpytz <theophiluschikezie@gmail.com>
2023-08-10 08:13:00 -0700

    Add new file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,24 +20,19 @@ int main(int ac, char *av[])
 	}
 	fd1 = open(av[1], O_RDONLY, 0);
 	if (fd1 == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
 	while ((n = read(fd1, buf, BUFSIZE)) > 0)
-		write(fd2, buf, n);
-	if (close(fd1) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
+		if (write(fd2, buf, n) != n)
+			dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+	if (close(fd1) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd1), exit(100);
 	if (close(fd2) == -1)
 		dprintf(2, "Error: Can't close fd %d\n", fd2), exit(100);
 	return (0);
