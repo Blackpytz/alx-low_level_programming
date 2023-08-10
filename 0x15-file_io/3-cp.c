31ae4d5022a7b0fc8569f788b8935f057e0ede49
Blackpytz <theophiluschikezie@gmail.com>
2023-08-10 07:12:49 -0700

    Add new file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-void copy_text(const char *file from, char *file_to);
-
 /**
-* main - main function
+* main - program that copies the content of a file to
+* another file
 * @ac: argument count
 * @av: argument vector
 *
@@ -11,10 +10,12 @@ void copy_text(const char *file from, char *file_to);
 */
 int main(int ac, char **av)
 {
+	int fd1, fd2, n;
+	char buf[BUFSIZE];
+	mode_t mode = S_IRUSR | S_IWURS | S_IRGRP | S_IWGRP | S_IROTH;
 	if (ac != 3)
 	{
 		dprinf(2, "Usage: cp file_from file_to");
 		exit(97);
 	}
-	copy 
 }
