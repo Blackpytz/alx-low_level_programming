953d9dd42ebf23e8dd9bd9c62286dab298aed090
Blackpytz <theophiluschikezie@gmail.com>
2023-08-10 15:10:43 -0700

    Add new file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,59 +3,58 @@
 void close_file(int fd);
 
 /**
-* main - program that copies the content of a file to
-* another file
+* main - program that copies the content of a file to another file.
 * @ac: argument count
 * @av: argument vector
-*
-* Return: Always 0.
+* Return: 0 on success.
 */
 int main(int ac, char *av[])
 {
-	int fd1, fd2, bytes_read, bytes_write;
-	char buf[BUFSIZE];
+	int fd_in, fd_out, bytes_read, bytes_write;
+	char buffer[BUFSIZE];
 
 	if (ac != 3)
-		dprintf(2, "Usage: cp file_from file_to"), exit(97);
-	fd1 = open(av[1], O_RDONLY);
-	if (fd1 == -1)
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	fd_in = open(av[1], O_RDONLY);
+	if (fd_in == -1)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd2 == -1)
+	fd_out = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_out == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		close(fd1);
+		close_file(fd_in);
 		exit(99);
 	}
-
-	while ((bytes_read = read(fd1, buf, BUFSIZE)) > 0)
+	while ((bytes_read = read(fd_in, buffer, BUFSIZE)) > 0)
 	{
-		bytes_write = write(fd2, buf, bytes_read);
+		bytes_write = write(fd_out, buffe, bytes_read);
 		if (bytes_write != bytes_read)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			close_file(fd1);
-			close_file(fd2);
+			close_file(fd_in);
+			close_file(fd_out);
 			exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		close_file(fd1);
-		close_file(fd2);
-		exit(98);
+		{
+			close_file(fd_in);
+			close_file(fd_out);
+			exit(98);
+		}
 	}
-	close_file(fd1);
-	close_file(fd2);
+	close_file(fd_in);
+	cose_file(fd_out);
 	return (0);
 }
 
 /**
-* close_file - function that close a file
+* close_file - function that closes a file
 * @fd: file descriptor
 *
-* Return: Void.
+* Return: void.
 */
 void close_file(int fd)
 {
