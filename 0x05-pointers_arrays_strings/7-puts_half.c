333a0b1028f1ea6b4794fd665135e16c73211542
Blackpytz <theophiluschikezie@gmail.com>
2023-10-19 00:48:15 +0000

    Add new files

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,25 +8,22 @@
 */
 void puts_half(char *str)
 {
-	int len = 0, n, i;
+	int len = 0, n = 0, i;
 
 	while (str[len])
 		len++;
 	if (len % 2 == 0)
 	{
 		n = len / 2;
-		for (i = n; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
 	}
 	else
 	{
 		n = (len - 1) / 2;
-		for (i = n; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
+	}
+
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
