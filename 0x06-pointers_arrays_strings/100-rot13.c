7f13258b71c136d2d8e55bfe35d76a9f7a36c95d
Blackpytz <theophiluschikezie@gmail.com>
2023-10-20 23:40:54 +0000

    Add new files

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* rot13 - Function that encodes a string using rot13.
+* @str: Pointer to a string.
+*
+* Return: A pointer to str.
+*/
+char *rot13(char *str)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
