1cc727fdb9a17e5ebf67ba6ba794f0a7dba4b452
Blackpytz <theophiluschikezie@gmail.com>
2023-10-23 18:42:11 +0000

    Add new file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	for (; i < n && src[i]; i++)
+	for (; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
 	return (dest);
