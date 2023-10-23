6c7df7188226bb0a02e1bf58df7656d789109720
Blackpytz <theophiluschikezie@gmail.com>
2023-10-23 18:39:55 +0000

    Add new file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memcpy - Function that copies memory area.
+* @dest: Pointer to the string to be filled.
+* @src: Pointer to the memoru area that fills the string dest.
+* @n: Number of bytes fom src to be copied.
+*
+* Return: A pinter to dest.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	for (; i < n && src[i]; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
