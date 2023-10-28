686d6a5aadceab25facf830bf316c78b8c7af022
Blackpytz <theophiluschikezie@gmail.com>
2023-10-28 00:08:27 +0000

    Add new files

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 
 /**
 * main - Program that prints the number of arguments passed into it.
-# @argc: Argument count.
+* @argc: Argument count.
 * @argv: argument vector.
 *
 * Return: 0 on success.
 */
-int main(int argc,char  __attribute__((unused))*argv[])
+int main(int argc, char *argv[]  __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 
