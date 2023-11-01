91a5c9a864ac978f71a7f5ff47cfebdcb69d674e
Blackpytz <theophiluschikezie@gmail.com>
2023-11-01 00:47:33 +0000

    Add update to .gitignore file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* create_array - Function that creates an array of chars,
+* and initializes it with a specific char.
+* @size: Size of the array.
+* @c: Character to be initialized.
+*
+* Return: ! pointer to the array, or NULL if it fails.
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc((sizeof(char) * size) + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	str[i] = '\0';
+
+	return (str);
+}
