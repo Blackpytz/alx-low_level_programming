49a495ce08a7b4e132a2121f35205360838f856f
Blackpytz <theophiluschikezie@gmail.com>
2023-11-02 16:11:00 +0000

    Add new files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,20 +19,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	size1 = strlen(s1);
-	if (n >= strlen(s2))
-		size2 = strlen(s2);
-	else
-		size2 = n;
+	size2 = strlen(s2);
 
-	str = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (n >= size2)
+		n = size2;
+
+	str = malloc(sizeof(char) * (size1 + n + 1));
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size1; i++)
 		str[i] = s1[i];
-	for (j = 0; j < size2; j++)
+
+	for (j = 0; j < n; j++)
 		str[i + j] = s2[j];
-	str[i + j + 1] = '\0';
+	str[i + j] = '\0';
 
 	return (str);
 }
