23f1c871d4def7d4fc9f37e72d34e45c1c572178
Blackpytz <theophiluschikezie@gmail.com>
2023-11-06 17:26:59 +0000

    Add readme file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+0x0E. C - Structures, typedef
+By: Julien Barbier
+Weight: 1
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+* struct dog - A structure with three elements.
+* @name: First element.
+* @age: Second element.
+* @owner: Third element.
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif /* DOG_H *?
