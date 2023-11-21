23c9576d219a5d23a063ce4385337c04abb5b5e7
Blackpytz <theophiluschikezie@gmail.com>
2023-11-21 21:21:08 +0000

    Add new files

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Function that frees a listint_t list.
+* @head: Pointer to a pointer that points to a struct.
+*
+* Return: Nothing.
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *aux, *curr;
+
+	curr = *head;
+	while (curr != NULL)
+	{
+		aux = curr;
+		curr = curr->next;
+		free(aux);
+	}
+
+	*head = NULL;
+}
