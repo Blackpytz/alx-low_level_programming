d256e51e0ea7d85cf53b3d0c7eba127e8d1d827d
Blackpytz <theophiluschikezie@gmail.com>
2024-01-13 13:30:29 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,21 @@
 */
 size_t print_list(const list_t *h)
 {
-    if (h != NULL)
-    {
-        size_t num = 0;
+	if (h != NULL)
+	{
+	size_t num = 0;
 
-        for (; h != NULL; h = h->next)
-        {
-            if (h->str == NULL)
-                puts("[0] (nil)");
-            else
-                printf("[%d] %s\n", h->len, h->str);
-            num++;
-        }
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str == NULL)
+			puts("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		num++;
+	}
 
-        return (num);
-    }
-        return (0);
+	return (num);
+	}
+
+	return (0);
 }
